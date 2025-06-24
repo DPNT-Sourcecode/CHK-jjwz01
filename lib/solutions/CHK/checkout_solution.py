0b862e25c083ea0e4ae86c423691d61603f8fa7c
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-06-24 13:02:48 +0000

    Tue Jun 24 13:02:48 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,31 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        return -1
+        basket = skus.split
+        Acount = 0
+        Bcount = 0
+        sum = 0
+        for i in range(basket):
+            if basket[i] == "A":
+                Acount += 1
+            elif basket[i] == "B":
+                Bcount += 1
+            elif basket[i] == "C":
+                sum += 20
+            elif basket[i] == "D":
+                sum += 15
+            else:
+                return -1
+        Amult = Acount//3
+        Asurp = Acount%3
+        Bmult = Bcount//2
+        Bsurp = Bcount%2
+        specials_sum = (Amult*130) + (Asurp*50) + (Bmult*45) + (Bsurp*30)
+        return specials_sum+sum
+        
         
             
+
 
 
 
