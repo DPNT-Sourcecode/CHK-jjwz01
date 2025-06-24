b174c62f18dca2281ca231390576fb81428d0c77
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-06-24 12:32:45 +0000

    Tue Jun 24 12:32:45 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,4 +2,13 @@
 class SumSolution:
     
     def compute(self, x, y):
-        raise NotImplementedError()
+        if(100<x<0 or 100<y<0):
+            return "invalid"
+        return x+y
+
+def compute(x, y):
+    if(100<x<0 or 100<y<0):
+        return "invalid"
+    return x+y
+
+compute(1+2)
