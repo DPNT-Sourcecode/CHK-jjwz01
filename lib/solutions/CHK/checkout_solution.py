4b1865a0535f2c1975c73437a19bf18d16dc5b09
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-06-24 13:18:23 +0000

    Tue Jun 24 13:18:23 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,24 +3,35 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        prices = {'A': 50, 'B': 30, 'C':20, 'D':15}
+        prices = {'A': 50, 'B': 30, 'C':20, 'D':15, 'E':40}
         basket = {}
         for item in skus:
             if item not in prices:
                 return -1
             basket[item] = basket.get(item, 0)+1
         total = 0
+        if 'E' in basket and 'B' in basket:
+            freeB = min(basket['E'] // 2, basket['B'])
+            basket['B'] -= freeB
         if 'A' in basket:
-            total += (basket['A']//3)*130 + (basket['A']%3)* prices['A']
+            basketA = basket['A']
+            total += (basket['A']//5)*200
+            basketA %= 5
+            total += (basket['A']//3)*130 
+            basketA %= 3
+            total += basket['A']* prices['A']
         if 'B' in basket:
             total += (basket['B']//2)*45 + (basket['B']%2)* prices['B']
         if 'C' in basket:
             total += basket['C'] * prices['C']
         if 'D' in basket:
             total += basket['D'] * prices['D']
+        if 'E' in basket:
+            totalk += basket['E'] * prices['E']
         return total
         
         
         
         
             
+
