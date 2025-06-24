063730a156a4d3ce6ef8245ac7fd7d0fabc02db0
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-06-24 12:51:07 +0000

    Tue Jun 24 12:51:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,13 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        if skus == 0:
+        if len.skus == 0:
             return -1
         sum = 0
         for i in range(skus):
-            sum += skus
+            sum += skus[i]
+            i+=1
         return sum
             
+
 
