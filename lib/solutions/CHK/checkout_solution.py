c5e5a374863b9795ef9d16096ed7bf55ff501007
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-06-24 13:10:28 +0000

    Tue Jun 24 13:10:28 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,32 +3,24 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        basket = skus.split
-        Acount = 0
-        Bcount = 0
-        sum = 0
-        for i in range(basket):
-            if basket[i] == "A":
-                Acount += 1
-            elif basket[i] == "B":
-                Bcount += 1
-            elif basket[i] == "C":
-                sum += 20
-            elif basket[i] == "D":
-                sum += 15
-            else:
+        prices = {'A': 50, 'B': 30, 'C':20, 'D':15}
+        basket = {}
+        for item in skus:
+            if item not in prices:
                 return -1
-        Amult = Acount//3
-        Asurp = Acount%3
-        Bmult = Bcount//2
-        Bsurp = Bcount%2
-        specials_sum = (Amult*130) + (Asurp*50) + (Bmult*45) + (Bsurp*30)
-        return specials_sum+sum
+            basket[item] = basket.get(item, 0)+1
+        total = 0
+        if 'A' in basket:
+            total += (basket['A']//3)*130 + (basket['A']%3)* prices['A']
+        if 'B' in basket:
+            total += (basket['B']//2)*45 + (basket['B']%2)* prices['B']
+        if 'C' in basket:
+            total += basket['C'] * prices['C']
+        if 'D' in basket:
+            total += basket['D'] * prices['D']
+        return total
+        
+        
         
         
             
-
-
-
-
-
